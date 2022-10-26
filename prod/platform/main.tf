3bb8f5050ae86b4edcac263e612b2e7f268cd599
Artyom Zverev <artisst03@gmail.com>
2022-10-26 17:53:35 +0300

    Test

---
+++ prod/platform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "4.96.0"
+    }
+  }
+
+  backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/40541314/terraform/state/platform"
+    lock_address   = "https://gitlab.com/api/v4/projects/40541314/terraform/state/platform/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/40541314/terraform/state/platform/lock"
+    username       = "gitlab-ci-token"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+  }
+}
