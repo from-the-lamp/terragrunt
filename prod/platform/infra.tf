dada5101317f5dea5a4f7d5961bec57dc9c7141e
Artyom Zverev <artisst03@gmail.com>
2022-10-26 18:43:49 +0300

    Test

--- prod/platform/infra.tf
+++ prod/platform/infra.tf
@@ -1,6 +1,10 @@
 provider "oci" {
   auth = "APIKey"
   region = var.region
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
 }
 
 module "vcn" {
